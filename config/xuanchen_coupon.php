fc9a813865188f9f5c08a5fa6b7d80ca93a424ad
xuanchen <122383162@qq.com>
2020-11-27 11:02:56 +0800

    增加规则

--- config/xuanchen_coupon.php
+++ config/xuanchen_coupon.php
@@ -3,13 +3,19 @@
 return [
     'coupon_model' => \App\Models\Coupon::class,
     'rules'        => [
-        'pingan' => [
-            'pattern' => '/^\d{12}$/',
-            'model'   => \XuanChen\Coupon\Action\PinganAction::class,
-        ],
-        'ysd'    => [
+        'ysd'      => [
             'pattern' => '/^YSD/',
             'model'   => \XuanChen\Coupon\Action\YsdAction::class,
         ],
+        'unionpay' => [
+            'pattern' => '/^66406/',
+            'model'   => \XuanChen\Coupon\Action\YsdAction::class,
+        ],
+        'pingan'   => [
+            'pattern' => '/^\d{12}$/',
+            'model'   => \XuanChen\Coupon\Action\PinganAction::class,
+        ],
+
     ],
 ];
+
