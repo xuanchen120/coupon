df39cde6667291bd9f2e22245fe3f964593c8ac5
xuanchen <122383162@qq.com>
2021-01-14 10:23:33 +0800

    增加银联券核销

--- config/xuanchen_coupon.php
+++ config/xuanchen_coupon.php
@@ -3,18 +3,21 @@
 return [
     'coupon_model' => \App\Models\Coupon::class,
     'rules'        => [
-        'ysd'    => [
-            //            'pattern' => '/^YSD\d{12}/',
+        'ysd'      => [
             'pattern' => '/^YSD/',
             'model'   => \XuanChen\Coupon\Action\YsdAction::class,
         ],
-        'pingan' => [
+        'unionpay' => [
+            'pattern' => '/^66406/',
+            'model'   => \XuanChen\Coupon\Action\YsdAction::class,
+        ],
+        'pingan'   => [
             'pattern' => '/^\d{12}$/',
             'model'   => \XuanChen\Coupon\Action\PinganAction::class,
         ],
 
     ],
-    
+
     'froms' => [
         'bsshop',//本时商城
         'bslive',//本时生活
